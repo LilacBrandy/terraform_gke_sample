3c5ef0061768d58f1e8552c85619ebf3f59f2bf3
JK Gunnink <jgunnink@gmail.com>
2020-02-03 21:52:11 +0800

    add default gke example with nginx

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "google" {
+  credentials = file("account.json")
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_container_cluster" "default" {
+  name        = "${var.name}"
+  project     = "${var.project}"
+  description = "GKE Cluster"
+  location    = "${var.region}"
+
+  remove_default_node_pool = true
+  initial_node_count       = "${var.initial_node_count}"
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "default" {
+  name       = "${var.name}-node-pool"
+  project    = "${var.project}"
+  location   = "${var.region}"
+  cluster    = "${google_container_cluster.default.name}"
+  node_count = "${var.default_node_count}"
+
+  node_config {
+    preemptible  = true # Change if your workloads are stateful
+    machine_type = "${var.machine_type}"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "default_node_count" {
+  default = 1
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "region" {
+  default = "australia-southeast1"
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
+
+variable "name" {
+  default = "sample-cluster"
+}
+
+variable "project" {
+  default = "<YOUR PROJECT ID HERE>"
+}
+
+variable "zone" {
+  default = "australia-southeast1-a"
+}
