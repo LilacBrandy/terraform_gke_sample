a1e3e3558afe40a9d7a88f5576e271a87ff19ccf
JK Gunnink <jgunnink@gmail.com>
2020-12-10 21:33:37 +0800

    fix deprecations

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "cluster_endpoint" {
-  value = "${google_container_cluster.default.endpoint}"
+  value = google_container_cluster.default.endpoint
 }
 
 output "cluster_master_version" {
-  value = "${google_container_cluster.default.master_version}"
+  value = google_container_cluster.default.master_version
 }
